7394419f6867b9b788dfaf0349df05104c73e4d3
jhkim988 <fccva3910@gmail.com>
2022-12-13 23:46:17 +0900

    세션 리스너 공부

--- DZBoard/src/main/java/server/SessionDBListener.java
+++ DZBoard/src/main/java/server/SessionDBListener.java
@@ -10,60 +10,76 @@ import jakarta.servlet.http.HttpSessionListener;
 
 @WebListener
 public class SessionDBListener implements HttpSessionListener, HttpSessionAttributeListener, HttpSessionActivationListener, HttpSessionBindingListener {
-	private int numVisit = 0;
-	private int numLogin = 0;
+	private static int numVisit = 0;
+	private static int numLogin = 0;
 	
 	
 	/* HttpSessionListener */
 	@Override
     public void sessionCreated(HttpSessionEvent se)  {
+		// 세션이 생성될 때 (브라우저로 처음 서버에 접속할 때)
 		numVisit++;
-		System.out.println("numVisit: " + numVisit);
+		System.out.println("sessionCreated - numVisit: " + numVisit);
     }
 
 	/* HttpSessionListener */
 	@Override
     public void sessionDestroyed(HttpSessionEvent se)  { 
+		// 브라우저를 종료해도 호출되지 않는다 -> 세션이 만료될 때 사라지는 듯
 		numVisit--;
-		System.out.println("numVisit: " + numVisit);
+		System.out.println("sessionDestroyed - numVisit: " + numVisit);
 	}
-	
+	/*-------------------------------*/
 	/* HttpSessionActivationListener */
+	/* 세션이 다른 VM 으로 마이그레이션 되면 호출된다. */
 	@Override
     public void sessionDidActivate(HttpSessionEvent se)  { 
-    }
+		System.out.println("sessionDidActivate");
+	}
 
 	/* HttpSessionActivationListener */
 	@Override
     public void sessionWillPassivate(HttpSessionEvent se)  { 
-    }
-	
+		System.out.println("sessionWillPassivate");
+	}
+	/*-------------------------------*/
 	/* HttpSessionAttributeListener */
 	@Override
     public void attributeReplaced(HttpSessionBindingEvent se)  { 
+		// 세션에 할당된 값이 바뀔 때(session.get("key"))
+		System.out.println("attributeReplace");
     }
 
 	/* HttpSessionAttributeListener */
 	@Override
     public void attributeAdded(HttpSessionBindingEvent se)  { 
+		// 세션에 값이 할당될 때(session.get("key"))
+		numLogin++;
+		System.out.println("attributeAdded - numLogin: " + numLogin);
     }
 
 	/* HttpSessionAttributeListener */
 	@Override
     public void attributeRemoved(HttpSessionBindingEvent se)  { 
+		// 세션에 값이 제거될 때(session.remove("key"))
+		numLogin--;
+		System.out.println("attributeRemoved: - numLogin: " + numLogin);
     }
-
+	/*-------------------------------*/
 	/* HttpSessionBindingListener */
+	/* HttpSessionBindingListener 구현 클래스는 @WebListener 나 web.xml 에 등록하지 않는다. */
 	@Override
-    public void valueUnbound(HttpSessionBindingEvent event)  { 
-    
+    public void valueBound(HttpSessionBindingEvent event)  { // binding 된 대상 세션, key 등을 얻을 수 있다.
+		// HttpSessionBindingListener 를 구현한 객체가 session.setAttribute() 로 바인딩 될 때 호출된다.
+		System.out.println("valueBound");
 	}
 	
 	/* HttpSessionBindingListener */
 	@Override
-    public void valueBound(HttpSessionBindingEvent event)  { 
-    
+    public void valueUnbound(HttpSessionBindingEvent event)  { // unbinding 된 대상 세션, key 등을 얻을 수 있다.
+		// HttpSessionBindingListener 를 구현한 객체가 session.removeAttribute() 로 바인딩 해제될 때 호출된다.
+		System.out.println("valueUnbound");
 	}
-
+	
 	
 }
